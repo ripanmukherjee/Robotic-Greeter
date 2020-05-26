cb212abd1fe00f291cc71b463a1c2373fd23c7dc
Somak Mukherjee <ripanmukherjee@gmail.com>
2020-05-26 19:38:15 -0400

    Modifying Speech_Start_End.py for removing the mp3 files and output files.

--- Development/Speech_Recognition_Code/Speech_Start_End.py
+++ Development/Speech_Recognition_Code/Speech_Start_End.py
@@ -14,7 +14,9 @@
 # NOTE 1 : This program can be run separately or as a stand alone program as follow for testing purpose :
 # >> python3 Speech_Start_End.py
 
+import os
 import sys
+import glob
 import pygame
 from gtts import gTTS
 from datetime import date, datetime
@@ -41,16 +43,31 @@ def process(mp3_filename, text):
         continue
 
 
+def delete_mp3_output_files():
+    mp3_files = glob.glob('*.mp3', recursive=True)
+    output_files = glob.glob('*_Output.txt', recursive=True)
+    for files in mp3_files:
+        try:
+            os.remove(files)
+        except OSError:
+            print("Cannot delete the old mp3 files.")
+
+    for files in output_files:
+        try:
+            os.remove(files)
+        except OSError:
+            print("Cannot delete the old output text files.")
+
+
 def main():
     today = date.today()
     current_date = today.strftime("%d/%m/%Y")
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print('Starting program : Speech_Start_End.py - at : ' + current_time + ' on : ' + current_date)
-
+    stand_alone_flag = None
     try:
         input_argv = sys.argv[1]
-
         if input_argv == "0":
             mp3_filename = "Speech_Start_End"
             text = "Hello, Welcome to Care Go, my name is TELIA, I am Care Go’s virtual greeter."
@@ -62,10 +79,15 @@ def main():
             text = "Hello " + input_argv + ". Welcome to Care Go, do you remember me, TELIA, Care Go’s " \
                                            "virtual greeter? "
     except IndexError:
+        stand_alone_flag = 1
         mp3_filename = "Speech_Start_End"
         text = "Hello, Welcome to Care Go, my name is TELIA, I am Care Go’s virtual greeter."
 
     process(mp3_filename, text)
+    if stand_alone_flag == 1:
+        print("Deleting mp3 and output file. Value of stand_alone_flag : ", str(stand_alone_flag))
+        delete_mp3_output_files()
+
     exit_program()
 
 
