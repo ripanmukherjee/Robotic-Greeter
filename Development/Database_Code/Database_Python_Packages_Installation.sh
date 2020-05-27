77221c4dae4b7943e852df3f3b05173f0ee72ea5
Somak Mukherjee <ripanmukherjee@gmail.com>
2020-05-27 14:35:37 -0400

    Adding Database_Python_Packages_Installation.sh for installing all the packages for Database_Code directory

--- Development/Database_Code/Database_Python_Packages_Installation.sh
+++ Development/Database_Code/Database_Python_Packages_Installation.sh
@@ -1,22 +1,25 @@
 #!/bin/ksh
 
-echo "Starting Installing regex........."
+echo "Starting Installing regex!!"
 pip3 install regex
-echo "Ending Installing regex........."
+echo "Ending Installing regex!!"
 
 echo "                                                                                                             "
 echo "============================================================================================================="
 echo "                                                                                                             "
 
-echo "Starting Installing os-sys........."
+echo "Starting Installing os-sys!!"
 pip3 install os-sys
-echo "Ending Installing os-sys........."
+echo "Ending Installing os-sys!!"
 echo "PLEASE IGNORE IF OS-SYS GIVING - ERROR: No matching distribution found for pywin32 (from wmi->os-sys)"
 
 echo "                                                                                                             "
 echo "============================================================================================================="
 echo "                                                                                                             "
 
-echo "Starting Installing subprocess........."
+echo "Starting Installing subprocess!!"
 pip3 install subprocess.run
-echo "Ending Installing subprocess........."
+echo "Ending Installing subprocess!!"
+
+echo "                                                                                                             "
+echo "Installation Done!!"
