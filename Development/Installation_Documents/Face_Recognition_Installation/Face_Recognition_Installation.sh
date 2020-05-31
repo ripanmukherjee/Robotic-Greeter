8a22de0a94c127e749b562e827a7d4de997fc8aa
Somak Mukherjee <ripanmukherjee@gmail.com>
2020-05-31 13:54:38 -0400

    Creating Face Recognition sh and README.rst

---
+++ Development/Installation_Documents/Face_Recognition_Installation/Face_Recognition_Installation.sh
@@ -0,0 +1,10 @@
+#!/bin/ksh
+
+echo "Starting Installing Face Recognition!!"
+pip3 install dlib
+pip3 install face_recognition
+pip3 install --upgrade imutils
+echo "Ending Installing Face Recognition!!"
+
+echo "                                                                                                             "
+echo "Installation Done!!"
