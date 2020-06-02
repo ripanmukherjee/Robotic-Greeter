00241b79107ddeb75a5d1706e59970e58f877ba7
Somak Mukherjee <ripanmukherjee@gmail.com>
2020-06-02 11:48:25 -0400

    Updating the folder

--- Development/Installation_Documents/Speech_Recognition_Installation/Speech_Recognition_Installation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/ksh
-
-echo "Starting Installing Speech Recognition!!"
-pip3 install gTTS
-pip3 install SpeechRecognition
-cho "Ending Installing Speech Recognition!!"
-
-echo "                                                                                                             "
-echo "Installation Done!!"
