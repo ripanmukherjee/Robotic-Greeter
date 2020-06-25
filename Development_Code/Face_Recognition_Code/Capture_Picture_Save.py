a5dd63c0e656f492426343b455a3b4d31b733d0c
Somak Mukherjee <ripanmukherjee@gmail.com>
2020-06-25 13:49:31 -0400

    Updating description - Putting description before each function - Capture_Picture_Save.py

--- Development_Code/Face_Recognition_Code/Capture_Picture_Save.py
+++ Development_Code/Face_Recognition_Code/Capture_Picture_Save.py
@@ -11,7 +11,7 @@
 #               argument (Unique ID) from the main process, and based on the argument, it will first take a picture of
 #               the person and later save it into the Dataset directory with the help of Unique ID. This program should
 #               save the picture into the following directory :
-#               * Dataset/XXX_UniqueID/XXX_UniqueID_YYY.jpg (XXX - Person Name, YYY - Instance)
+#               * Dataset/XXX_UniqueID/XXX_UniqueID_YYY.jpg (XXX - Person Name, YYY - Serial number)
 # **********************************************************************************************************************
 # NOTE 1:       Please check following line of code:
 #               cap = cv2.VideoCapture(-1) - If with -1 video stream is not working then you can replace with 0 or 1.
@@ -33,6 +33,12 @@ from subprocess import check_output
 
 
 def start_program():
+    """
+    ************************************ Inside start_program function *************************************************
+    This function will be called at the beginning to print today's date and start time.
+    ************************************ Inside start_program function *************************************************
+    """
+
     today = date.today()
     current_date = today.strftime("%d/%m/%Y")
     now = datetime.now()
@@ -41,6 +47,12 @@ def start_program():
 
 
 def exit_program():
+    """
+    ************************************ Inside exit_program function **************************************************
+    This function will be called at the end to print today's date and end time.
+    ************************************ Inside exit_program function **************************************************
+    """
+
     today = date.today()
     current_date = today.strftime("%d/%m/%Y")
     now = datetime.now()
@@ -49,7 +61,31 @@ def exit_program():
     sys.exit()
 
 
+def process_parameter_set():
+    """
+    ************************************ Inside process_parameter_set function *****************************************
+    This function will be called to set the essential parameter needed for this program as below:
+
+    1. dataset_path signifies the folder, where all the new images will be save.
+    2. face_cascade signifies OpenCV cascade classifier from haarcascade_frontalface_default.xml file.
+    So, it is essential to verify the parameter before running this process.
+    ************************************ Inside process_parameter_set function *****************************************
+    """
+
+    dataset_path = 'Dataset'
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    return dataset_path, face_cascade
+
+
 def process_get_details():
+    """
+    ************************************ Inside process_get_details function *******************************************
+    This function will be called when the customer takes their pictures. It will prompt a pop-up message where the
+    customers will write their names so that the photos will be saved with the name mentioned by the customer.
+    ************************************ Inside process_get_details function *******************************************
+    """
+
     details = None
     args_get_details = "zenity --forms --width=500 --height=200 --title='Save picture with a valid name' \
                         --text='Type only the First Name' \
@@ -66,9 +102,21 @@ def process_get_details():
     return details
 
 
-def process_save_picture(details, img, fps, unique_id):
+def process_save_picture(details, img, fps, unique_id, dataset_path):
+    """
+    ************************************ Inside process_save_picture function ******************************************
+    This function will first check the dataset_path folder is present or not. If not, then it will exit from the
+    program. If the dataset_path folder exists, then it will check if any folder with the same name and Id is present
+    inside of the dataset_path or not. If Yes, then it will just save the image inside of it, and if not, then it will
+    create a new folder and save the image inside it. Concept of folder name and image name as follow:
+
+    1. Folder Name - XXX_UniqueID (XXX - Person Name)
+    2. Image Name  - XXX_UniqueID_YYY.jpg (YYY - Serial Number, this number will increase by 1 automatically, if the
+                     customer wants more than 1 photo.
+    ************************************ Inside process_save_picture function ******************************************
+    """
+
     details = details + '_' + unique_id
-    dataset_path = 'Dataset'
     path = Path(dataset_path)
     if path.exists() is False:
         print("ERROR : path.exits() is false - Directory : Dataset is not present - "
@@ -102,8 +150,14 @@ def process_save_picture(details, img, fps, unique_id):
             exit_program()
 
 
-def process_picture_taking(unique_id):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def process_picture_taking(unique_id, dataset_path, face_cascade):
+    """
+    ************************************ Inside process_picture_taking function ****************************************
+    This function is the primary function that will open the camera and sill capture the image from the camera. Later,
+    it will call process_save_picture function to save the picture.
+    ************************************ Inside process_picture_taking function ****************************************
+    """
+
     cap = cv2.VideoCapture(0)
     time.sleep(2.0)
     capture_duration = 3
@@ -122,7 +176,7 @@ def process_picture_taking(unique_id):
 
             if int(end_time) - int(start_time) > capture_duration:
                 details = process_get_details()
-                process_save_picture(details, img, fps, unique_id)
+                process_save_picture(details, img, fps, unique_id, dataset_path)
 
             for ((top, right, bottom, left), name) in zip(boxes, names):
                 cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
@@ -147,23 +201,37 @@ def process_picture_taking(unique_id):
     cap.release()
 
 
-def process_check_input_argument():
+def process_check_input_argument(dataset_path, face_cascade):
+    """
+    ************************************ Inside process_check_input_argument function **********************************
+    This function will receive an input argument (Unique ID) from the Main_Process and pass the case to the main
+    process_calling function. If this function does not receive any argument, it will pass "0" as an argument.
+    ************************************ Inside process_check_input_argument function **********************************
+    """
+
     try:
         print('Inside Capture_Picture_Save.py - Unique ID is : ', sys.argv[1])
         print('Processing Capture_Picture_Save.py from Capture_Picture_Main.py.')
         unique_id = sys.argv[1]
-        process_picture_taking(unique_id)
+        process_picture_taking(unique_id, dataset_path, face_cascade)
         exit_program()
     except IndexError:
         print("Processing Capture_Picture_Save.py stand alone")
         unique_id = "0"
-        process_picture_taking(unique_id)
+        process_picture_taking(unique_id, dataset_path, face_cascade)
         exit_program()
 
 
 def main():
+    """
+    ************************************ Inside main function **********************************************************
+    This is the main process which will call at the very beginning and will call the other functions.
+    ************************************ Inside main function **********************************************************
+    """
+
     start_program()
-    process_check_input_argument()
+    dataset_path, face_cascade = process_parameter_set()
+    process_check_input_argument(dataset_path, face_cascade)
     exit_program()
 
 
