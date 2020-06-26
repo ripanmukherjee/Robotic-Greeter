9e0c04636df751721688cfe8dadccf9a09c495aa
Somak Mukherjee <ripanmukherjee@gmail.com>
2020-06-25 21:35:32 -0400

    Updating description - Adding description before each function - Customer_Search_Name.py

--- Development_Code/Database_Code/Customer_Search_Name.py
+++ Development_Code/Database_Code/Customer_Search_Name.py
@@ -76,12 +76,28 @@ def exit_program():
 
 
 def process_parameter_set():
+    """
+    ************************************ Inside process_parameter_set function *****************************************
+    This function will be called to set the essential parameter needed for this program as below:
+
+    1. Region = "DEV" signifies that we are running the code in the development region. And as per the region value,
+    this program will choose the table. So, it is essential to set region value correctly.
+    ************************************ Inside process_parameter_set function *****************************************
+    """
+
     region = "DEV"
 
     return region
 
 
 def process_checking_region_table(region):
+    """
+    ************************************ Inside process_checking_region_table function *********************************
+    This function will be called to get main_table and sequence_table as per region value. If the region value is not
+    set correctly, then this function will give error and will exit from the program.
+    ************************************ Inside process_checking_region_table function *********************************
+    """
+
     main_table = None
     sequence_table = None
     if region == "DEV":
@@ -102,6 +118,13 @@ def process_checking_region_table(region):
 
 
 def process_get_details():
+    """
+    ************************************ Inside process_get_details function *******************************************
+    This function will ask the customer to enter the details for either First or Last Name or both and later will
+    return the details. If the function cannot able to call the pop-up function, then it will exit from the program.
+    ************************************ Inside process_get_details function *******************************************
+    """
+
     details = None
     args_get_details = "zenity --forms --width=500 --height=200 --title='Search user' \
                 --text='Search with either First or Last Name or both of the following' \
@@ -119,6 +142,17 @@ def process_get_details():
 
 
 def process_format_details(details):
+    """
+    ************************************ Inside process_format_details function ****************************************
+    This function will validate the details entered by the customers. The Name cannot be blank or less than 2
+    characters. And later will return it as below:
+
+    1. If the customer entered both First and Last Name, then it will both value.
+    2. If the customer entered only First Name, it will return the First Name and will return the Last Name as Space.
+    3. If the customer entered only Last Name, it will return the Last Name and will return the First Name as Space.
+    ************************************ Inside process_format_details function ****************************************
+    """
+
     first_name = None
     last_name = None
     details[1] = details[1].strip()
@@ -150,6 +184,13 @@ def process_format_details(details):
 
 
 def process_search_first_name(check_main_table, first_name):
+    """
+    ************************************ Inside process_search_first_name function *************************************
+    This function will search the details by only First Name. If there are no details in the table, it will prompt
+    as "There are no data as per your search!!!!".
+    ************************************ Inside process_search_first_name function *************************************
+    """
+
     try:
         conn = psycopg2.connect(dbname="caregodb", user="postgres", password="postgres", host="127.0.0.1", port="5432")
         cur = conn.cursor()
@@ -178,6 +219,13 @@ def process_search_first_name(check_main_table, first_name):
 
 
 def process_search_last_name(check_main_table, last_name):
+    """
+    ************************************ Inside process_search_last_name function **************************************
+    This function will search the details by only Last Name. If there are no details in the table, it will prompt
+    as "There are no data as per your search!!!!".
+    ************************************ Inside process_search_last_name function **************************************
+    """
+
     try:
         conn = psycopg2.connect(dbname="caregodb", user="postgres", password="postgres", host="127.0.0.1", port="5432")
         cur = conn.cursor()
@@ -206,6 +254,13 @@ def process_search_last_name(check_main_table, last_name):
 
 
 def process_search_first_last_name(check_main_table, first_name, last_name):
+    """
+    ************************************ Inside process_search_first_last_name function ********************************
+    This function will search the details by First and Last Name. If there are no details in the table, it will prompt
+    as "There are no data as per your search!!!!".
+    ************************************ Inside process_search_first_last_name function ********************************
+    """
+
     try:
         conn = psycopg2.connect(dbname="caregodb", user="postgres", password="postgres", host="127.0.0.1", port="5432")
         cur = conn.cursor()
@@ -235,6 +290,12 @@ def process_search_first_last_name(check_main_table, first_name, last_name):
 
 
 def process_display_details(search_details):
+    """
+    ************************************ Inside process_display_details function ***************************************
+    This function will receive the search details and display it as a pop-up list.
+    ************************************ Inside process_display_details function ***************************************
+    """
+
     display_row = ""
     counter = 0
     for list_details in search_details:
@@ -263,6 +324,12 @@ def process_display_details(search_details):
 
 
 def process_search_display(check_main_table, first_name, last_name):
+    """
+    ************************************ Inside process_search_display function ****************************************
+    This function will call the search function based on the value of First and Last Name
+    ************************************ Inside process_search_display function ****************************************
+    """
+
     if first_name != "" and last_name == "":
         search_details = process_search_first_name(check_main_table, first_name)
     elif first_name == "" and last_name != "":
@@ -296,6 +363,13 @@ def process_search_display(check_main_table, first_name, last_name):
 
 
 def process_show_details(first_name, last_name, email_id, phone_no):
+    """
+    ************************************ Inside process_show_details function ******************************************
+    This function will show the Name of the person, Email ID and Phone Number as a pop-uo message after the customer
+    selected the search person
+    ************************************ Inside process_show_details function ******************************************
+    """
+
     try:
         check_output(["zenity", "--info", "--width=400", "--height=200", "--text=You have searched : "
                       + first_name + " " + last_name + "\n\nEmail ID is : " + email_id +
@@ -306,6 +380,12 @@ def process_show_details(first_name, last_name, email_id, phone_no):
 
 
 def process_write_pickle_file(first_name, last_name, email_id, phone_no):
+    """
+    ************************************ Inside process_write_pickle_file function *************************************
+    This function will write the details of the searched person into a pickle file
+    ************************************ Inside process_write_pickle_file function *************************************
+    """
+
     new_data = {"First_Name": first_name, "Last_Name": last_name, "Email_ID": email_id, "Phone_No": phone_no}
     with open("Search_Output.pickle", "wb+") as pickle_file:
         pickle_file.write(pickle.dumps(new_data))
