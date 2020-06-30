3d1b91be8da0324854fe50b75c7eab4c9ebf5c30
Somak Mukherjee <ripanmukherjee@gmail.com>
2020-06-30 14:23:20 -0400

    Updating Speech_Question.py

--- Development_Code/Speech_Recognition_Code/Speech_Question.py
+++ Development_Code/Speech_Recognition_Code/Speech_Question.py
@@ -172,7 +172,7 @@ def process_speak_listen(device_index, mp3_filename, text, record, flag):
                 record.adjust_for_ambient_noise(source)
                 print("Speak:")
                 os.system("zenity --progress --width=400 --height=200 --title='Speak Now' "
-                          "--text='Speak Now......No need to click Ok button' --no-cancel &")
+                          "--text='Speak Now......No need to click OK button' --no-cancel &")
                 try:
                     audio = record.listen(source, timeout=5)
                     record_text = record.recognize_google(audio)
@@ -241,9 +241,9 @@ def process_input_details(device_index, input_details, mp3_filename, record, yes
 
     response = "NO"
     if input_details is None:
-        text = "Sorry, I did not get an input from you."
         flag = 1
-        process_speak_listen(device_index, mp3_filename, text, record, flag)
+        # text = "Sorry, I did not get an input from you."
+        # process_speak_listen(device_index, mp3_filename, text, record, flag)
         response = "NO"
     else:
         tokenized_word = word_tokenize(input_details)
