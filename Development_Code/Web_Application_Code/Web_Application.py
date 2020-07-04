7a718a8b78bda87ee821ba52f7efe18f467e23d7
Somak Mukherjee <ripanmukherjee@gmail.com>
2020-07-04 15:57:21 -0400

    Updating Web_Application.py

--- Development_Code/Web_Application_Code/Web_Application.py
+++ Development_Code/Web_Application_Code/Web_Application.py
@@ -16,5 +16,40 @@ def about_carego():
     return render_template('/About_Carego.html')
 
 
+@app.route('/company/about-TELIA')
+def about_telia():
+    return render_template('/About_TELIA.html')
+
+
+@app.route('/company/emergency-evacuation-procedure')
+def emergency_evacuation_procedure():
+    return "emergency-evacuation-procedure"
+
+
+@app.route('/company/register-new-user')
+def register_new_user():
+    return render_template('/Register_New_User.html')
+
+
+@app.route('/company/modify-user')
+def modify_user():
+    return render_template('/Modify_User.html')
+
+
+@app.route('/company/search')
+def search():
+    return render_template('/Search.html')
+
+
+@app.route('/company/contact-us')
+def contact_us():
+    return render_template('/Contact_US.html')
+
+
+@app.route('/company/admin-login')
+def admin_login():
+    return render_template('/Admin_Login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4000)
